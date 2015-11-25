a0b9b6e6f9a31173447a9e4cc143d54d64dc4297
vampuk <vampuk@yandex.ru>
2015-11-26 00:31:53 +0300

    Update server.c
    3 потока, после выполнения команды переходят в режим ожидания.
    Если поток в режиме ожидания и поступила команда, то его выводят из режима ожидания и он выполняет её.

--- server.c
+++ server.c
@@ -1,17 +1,130 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <pthread.h>
+
 #define PORT 8080
 #define BACKLOG 10
+#define THREADS 3
 
-void InitServer()
+pthread_t	threads[THREADS];
+int		tasks[THREADS];
+pthread_mutex_t	locks[THREADS];
+
+
+void connection_processor(int cd)
+{
+		printf("Socket acepted [%d]\n", cd);
+				
+		while (1)
+		{
+			char *msg = "User@ssh$ \0";
+			send(cd, msg, strlen(msg), 0);			
+			
+			// Принятие команды
+			char cmd[1000];
+			memset(cmd, '\0', sizeof(cmd));
+			recv(cd, &cmd, sizeof(cmd), 0);
+			cmd[strlen(cmd) - 2] = '\0';	// Удалить \r\n
+
+			// Печать команды на сервер
+			printf("[command]: %s\n", cmd);
+
+			// Перечень команд
+			if (strcmp(cmd, "hello\0") == 0)
+			{				
+				char *msg = "Hello!\r\n\0";
+				send(cd, msg, strlen(msg), 0);
+			}
+			else if (strcmp(cmd, "help\0") == 0)
+			{
+				char *msg = "help,hello,ls,exit...\r\n\0";
+				send(cd, msg, strlen(msg), 0);
+				
+			}
+			else if (strcmp(cmd, "exit\0") == 0)
+			{
+				// Отключение
+				char *msg = "Close!\r\n\0";
+				send(cd, msg, strlen(msg), 0);
+				
+				close(cd);
+				break;
+			}
+			else
+			{
+				// Остальные команды (ls...)
+				FILE *fp = popen(cmd, "r");
+				if (fp == NULL) {
+				char *msg = "Failed to run command\r\n\0";
+				send(cd, msg, strlen(msg), 0);
+				}
+	
+				// Чтение, печать в сокет
+				char buf[1000];
+				while (fgets(buf, sizeof(buf), fp) != NULL) {
+				send(cd, buf, strlen(buf), 0);
+				}
+	
+				pclose(fp);
+			}	
+		}
+		
+		printf("Socket closed [%d]\n", cd);
+}
+
+void *thread_function(void *arg)
+{
+	int id = (int)arg;
+
+	while (1)
+	{		
+		printf("[info]: Thread %d waiting for task\n", id);			
+
+		pthread_mutex_lock(&locks[id]);
+		pthread_mutex_lock(&locks[id]);
+		pthread_mutex_unlock(&locks[id]);
+		
+		printf("[info]: Thread %d start task\n", id);
+		
+		// Выполнить
+		connection_processor(tasks[id]);
+	}	
+	
+	return 0;
+}
+
+void pool_give_task(int task)
+{
+	int i;
+	for (i = 0; i < THREADS; i++)
+	{
+		if (pthread_mutex_trylock(&locks[i]))	//Если закрыт, то не можем залочить
+		{
+			tasks[i] = task;
+			pthread_mutex_unlock(&locks[i]);
+			return;
+		}
+		else	//Если был открыт, то открыть еще раз
+		{
+			pthread_mutex_unlock(&locks[i]);
+		}
+	}
+	
+	close(task);	//Закрыть подключение
+	printf("[info]: All threads are bisy.\n");
+}
+
+void start_server()
 {
 	int ld = socket(AF_INET, SOCK_STREAM, 0);
 	if (ld == -1)
 	{
-		printf("[EXCEPTION]: Socket returns error!\n");
+		printf("[error]: Listener create error\n");
 	}
 
 	struct sockaddr_in serv_addr;
@@ -21,16 +134,16 @@ void InitServer()
 	
 	socklen_t size_saddr;
 	
-	int bindId = bind(ld, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	if (bindId == -1)
+	int bindRes = bind(ld, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (bindRes == -1)
 	{
-		printf("[EXCEPTION]: Bind returns error!\n");
+		printf("[error]: Bind error\n");
 	}
 	
-	int listenId = listen(ld, BACKLOG);
-	if (listenId == -1)
+	int listenRes = listen(ld, BACKLOG);
+	if (listenRes == -1)
 	{
-		printf("[EXCEPTION]: Listen returns error!\n");
+		printf("[error]: Listen error\n");
 	}
 	
 	while (1)
@@ -41,16 +154,28 @@ void InitServer()
 		int cd = accept(ld, (struct sockaddr *)&client_addr, &size_caddr);
 		if (cd == -1)
 		{
-			printf("[EXCEPTION]: Accept client error!\n");
+			printf("[error]: Accept error\n");
 		}
-		printf("workd");
-		break;
-		//TODO action with client
+		
+		pool_give_task(cd);
 	}
 }
 
 int main()
-{
-	InitServer();
+{	
+	int i;
+	for(i = 0; i < THREADS; i++)
+	{
+		pthread_mutex_init(&locks[i], NULL);
+		pthread_create(&threads[i], NULL, thread_function, (void *)(long long)i);
+	}
+
+	start_server();
+	//не обязательно
+	for(i = 0; i < THREADS; i++)
+	{
+		pthread_kill(threads[i], 0);
+		pthread_mutex_destroy(&locks[i]);
+	}
 	return 0;
 }
